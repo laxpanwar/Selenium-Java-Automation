5c7d1de44ce1d233d67b3de4fb1693b36d9bfaca
Laxmi Panwar <laxpanwar11@gmail.com>
2026-01-13 20:08:07 +0530

    Updated code. Added new page and test class.

--- src/test/java/tests/DashboardTest.java
+++ src/test/java/tests/DashboardTest.java
@@ -18,15 +18,34 @@ public class DashboardTest extends BaseTest {
     @Description("Usr is on dashboard page")
     public void verifyDashboardText() {
         Assert.assertEquals(dashboardPage.getDashboardText(), "Dashboard");
+
     }
+
+    @Test
+    @Description("Clicking on the PIM text from the menu")
+    public void verifyClickPimnBtn(){
+        dashboardPage.clickPimText();
+        dashboardPage.clickAddBtn();
+        Assert.assertEquals(dashboardPage.getPimHeaderText(), "PIM", "actual text is not matching with expected text");
+       // dashboardPage.uploadProfileImage();
+        dashboardPage.createEmployee();
+        dashboardPage.getEmployeeHeaderText();
+    }
+
+
+
+
+
 
     @Test
     @Description("Clicking on the admin button from the menu")
     public void verifyClickAdminBtn() throws InterruptedException {
         dashboardPage.clickAdminBtn();
-        Assert.assertEquals(dashboardPage.getAdminHeaderText(), "Admin", "actual text is not matching with expected text");
+        Assert.assertEquals(dashboardPage.getAdminHeaderText(),
+                "Admin", "actual text is not matching with expected text");
         dashboardPage.enterSystemUsername();
         dashboardPage.clickUserRoleDropdown();
+        dashboardPage.enterEmployeeNameUntilMatch();
 
     }
 
